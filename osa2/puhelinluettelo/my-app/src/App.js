1752345368244944a8f6ac37711aee8247e10a42
OnniVirtanen <onni.virtanen5@gmail.com>
2022-11-15 20:44:16 +0200

    Lisätty

--- osa2/puhelinluettelo/my-app/src/App.js
+++ osa2/puhelinluettelo/my-app/src/App.js
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Persons from './components/Persons'
 import FilterInput from './components/FilterInput'
 import PersonForm from './components/PersonForm'
+import axios from 'axios'
+
 
 const App = (props) => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+  console.log('effect')
+  axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    })
+  }, [])
+  console.log('render', persons.length, 'notes')
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -61,19 +69,13 @@ const App = (props) => {
   return (
     <div>
       <h2>Phonebook</h2>
-
         <FilterInput handleFilterChange={handleFilterChange} />
-
       <h2>Add a new</h2>
-
         <PersonForm addName={addName} newName={newName} newNumber={newNumber}
          handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}
          />
-
       <h2>Numbers</h2>
-
       <Persons persons={persons} completedFilterArray={completedFilterArray}/>
-      
     </div>
   )
 }
